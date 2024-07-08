e18200efebca79eb798af7000c0468be91d23221
Aníbal <anibal@myhotel.cl>
2024-07-08 12:42:20 -0400

    Add layout and sidebartemplate

--- src/app/presentation/layouts/DashboardLayout/DashboardLayout.component.ts
+++ src/app/presentation/layouts/DashboardLayout/DashboardLayout.component.ts
@@ -1,15 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import SidebarMenuItemComponent from '../../components/sidebarMenuItem/sidebarMenuItem.component';
+import { routes } from '../../../app.routes';
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    SidebarMenuItemComponent,
   ],
   templateUrl: './DashboardLayout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class DashboardLayoutComponent { }
+export default class DashboardLayoutComponent {
+  public routes = routes[0].children?.filter((route) => route.data);
+}
