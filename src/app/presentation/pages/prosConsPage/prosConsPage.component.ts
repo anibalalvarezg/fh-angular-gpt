7b0dc6bd96bacb34a0fcb1453c7b36eecd338deb
Aníbal <anibal.alvarezg@gmail.com>
2024-07-09 16:56:31 -0400

    pros cons page

--- src/app/presentation/pages/prosConsPage/prosConsPage.component.ts
+++ src/app/presentation/pages/prosConsPage/prosConsPage.component.ts
@@ -1,13 +1,49 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  TextMessageEvent,
+  TextMessageBoxEvent,
+  ChatMessageComponent,
+  MyMessageComponent,
+  TextMessageBoxComponent,
+  TypingLoaderComponent,
+} from '@components/index';
+import { Message } from 'app/interfaces';
+import { OpenAiService } from 'app/presentation/services/openai.service';
 
 @Component({
   selector: 'app-pros-cons-page',
   standalone: true,
   imports: [
     CommonModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
   ],
   templateUrl: './prosConsPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ProsConsPageComponent { }
+export default class ProsConsPageComponent {
+  messages = signal<Message[]>([]);
+  isLoading = signal(false);
+  openAiService = inject(OpenAiService);
+
+  handleMessage(prompt: string) {
+    console.log({ prompt });
+  }
+
+  handleMessageWithFile(event: TextMessageEvent) {
+    console.log({ event });
+  }
+
+  handleMessageWithSelect(event: TextMessageBoxEvent) {
+    console.log({ event });
+  }
+}
